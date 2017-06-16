f91d2be5e0b278d727929cbeb6c949ea5ef54230
Xiang Sun <tsiangsun@gmail.com>
2017-06-16 11:28:40 -0700

    update 6/16/17

--- app.py
+++ app.py
@@ -1,78 +1,66 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, redirect, render_template, url_for, request
+import requests
+import simplejson as json
+import numpy as np 
 import pandas as pd
-import numpy as np
-import requests 
-import simplejson
-import datetime
-import dateutil
-import io
-from bokeh.charts import Line
-from bokeh.embed import components
-#from bokeh.models import SingleIntervalTicker, LinearAxis
+from bokeh.plotting import figure
+from bokeh.embed import components 
 
 app = Flask(__name__)
+
+app.vars={}
 
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+    
+@app.route('/graph', methods=['POST'])
+def graph():
+    app.vars['symbol'] = request.form['symbol']
+    symbol = app.vars['symbol']
+    API_key='no_7haifmMMUdYQPyt_Q'
+    api_url='http://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=%s'%(symbol, API_key)
 
-@app.route('/index', methods=['POST'])
-def doSomething():
-  if request.form['symbol']:
-	if request.method == 'POST':
-		print 'Got POST request that looks like ' + request.form['symbol']
-		#api_addr = 'https://www.quandl.com/data/WIKI/'
-		api_addr = 'https://www.quandl.com/api/v3/datasets/WIKI/'
-		file_key = request.form['symbol'] + '.csv'
-		my_api_key = 'W8o1M8Y_wPyXnjs-HQPq'
-		end_date = datetime.date.today()
-		numMonths = -1
-		if request.form['timeframe'] == 'three': numMonths = -3
-		elif request.form['timeframe'] == 'twelve': numMonths = -12
-		start_date = end_date + dateutil.relativedelta.relativedelta(months=numMonths)
-		payload = {'api_key':my_api_key, 'start_date':start_date, 'end_date':end_date, 'column_index':'4'}
-		try:
-			r = requests.get(api_addr + file_key, params=payload)
-			closes_df = pd.read_csv(io.StringIO(r.content.decode('utf-8'))).sort_index(by='Date')
-			plot = Line(closes_df, x='Date', y='Close', title='Close v. Date for ' + request.form['symbol'], 
-			plot_width=1000)#, x_axis_type=None)
-# Try to figure out how to change the ticker size on the x axis someday when my internet works. 
-			if numMonths==1:
-				tick_size_big=1
-				tick_size_small=0
-				dnt = 31
-			elif numMonths==3:
-				tick_size_big=12
-				tick_size_small=7
-				dnt = 12
-			elif numMonths==12:
-				tick_size_big=12
-				tick_size_small=4
-				dnt = 12
-			#plot.models.ticker.desired_num_ticks = dnt
-			#ticker = SingleIntervalTicker(interval=tick_size_big, num_minor_ticks=tick_size_small)
-			#xaxis = LinearAxis(ticker=ticker)
-			#plot.add_layout(xaxis, 'below')
+    session=requests.Session()
+    session.mount('http://',requests.adapters.HTTPAdapter(max_retries=3))
+    raw_data = session.get(api_url)
+    if raw_data.status_code != 200 : 
+        print 'Fail in getting data'
 
-			script, div = components(plot)
-			return result(request.form['symbol'], script, div)
-		except: 
-			return incorrect_code()
-	else:
-		return 'request method was a GET apparently' 
+    df = pd.DataFrame(data=np.array(raw_data.json()['dataset']['data']), columns=raw_data.json()['dataset']['column_names'])
 
-@app.route('/err')
-def incorrect_code():
-	return render_template('err.html')
+    dfdate = pd.to_datetime(df['Date'])
 
+    p = figure(title='Stock prices for %s' % symbol ,x_axis_label='Date', x_axis_type='datetime')
 
-@app.route('/result/<tickerSymbol>')
-def result(tickerSymbol, bokeh_script, bokeh_div):
-  return render_template('result.html', symbol=tickerSymbol, b_s_=bokeh_script, b_d_=bokeh_div)
+    if request.form.get('Close'):
+        colname='Close'
+    elif request.form.get('Adj. Close'):
+        colname='Adj. Close'
+    elif request.form.get('Open'):
+        colname='Open'
+    elif request.form.get('Adj. Open'):
+        colname='Adj. Open'
+    else:  
+        return redirect('/error')
+
+    p.line(x=dfdate.values, y=df[colname].values, line_width=2, line_color="purple", legend=colname)
+    #show(p)
+
+    script, div = components(p)
+    return render_template('graph.html', script=script, div=div)
+
+@app.route('/error')
+def error():
+    return render_template('error.html', stock=app.vars['symbol'])
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=33507)
+    app.run(port=33507)
+
+
+
